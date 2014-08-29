3ec19cc8c5f14bb8557f8757e831671d7ed6be33
Gean C Rotta <gean.rotta@gmail.com>
2014-08-28 22:22:43 -0300

    V0.0.3
    Criação arquivos index paginas

---
+++ Home/index.php
@@ -0,0 +1,36 @@
+﻿<!-- Arquivo index php para paginas inf unoesc -->
+
+<!-- Inicio painel banners -->
+<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+  				<!-- Indicators -->
+  			<ol class="carousel-indicators">
+    			<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+    			<li data-target="#carousel-example-generic" data-slide-to="1"></li>
+    			
+  			</ol>
+
+  				<!-- Wrapper for slides -->
+  			<div class="carousel-inner">
+  			  <div class="item active">
+      			<img src="Imagens/banner_site_virtual_l_extensao_ead.png" alt="...">
+      				<div class="carousel-caption">
+      				</div>
+    		  </div>
+    		  <div class="item" align="center">
+      			<img src="Imagens/banner_novo.jpg" alt="..." height="100%" width="100%">
+      			<div class="carousel-caption">
+        			<h1 align="center"></h1>
+        			<h3 align="center"></h3>
+      			</div>
+    		  </div>
+  		   </div>
+           
+  			 <!-- Controls -->
+  		<a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+    		<span class="glyphicon glyphicon-chevron-left"></span>
+  		</a>
+  		<a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+    		<span class="glyphicon glyphicon-chevron-right"></span>
+  		</a>
+	</div>
+<!-- Fim painel de banners -->
