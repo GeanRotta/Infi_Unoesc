8af55dca57a2cf2700c0cd9eb4da1b05b0ff82b6
Gean C Rotta <gean.rotta@gmail.com>
2014-09-01 16:46:27 -0300

    V0.0.5
    Formulario de contatoe envio de email.

--- Contato/index.php
+++ Contato/index.php
@@ -1,2 +1,77 @@
-﻿<!-- Arquivo index php para paginas inf unoesc -->
-contato
+﻿<!-- 
+<div class="alert alert-success" role="alert">E-mail enviado com sucesso!</div>
+<div class="alert alert-danger" role="alert">Erro ao enviar E-mail!</div>
+-->
+<?php
+$destino = "tiago.zonta@unoesc.edu.br";
+  if(isset($_POST['submit'])){
+	$nome = $_POST['nome'];
+	$email = $_POST['email'];
+	$assunto = $_POST['assunto'];
+	$mensagem = $_POST['msg'];
+	if(mail($destino,$assunto,$mensagem,"From: $nome, E-mail: $email")){?>
+	  <div class="alert alert-success" role="alert">E-mail enviado com sucesso!</div>
+	<?php
+    }else{?>
+	  <div class="alert alert-danger" role="alert">Erro ao enviar E-mail!</div>
+	<?php
+	}
+  }
+
+?>
+
+
+<!-- codigo php acima e tals-->
+<!-- Arquivo index php para paginas inf unoesc -->
+<form class="form-horizontal" method="post" action="">
+    <fieldset>
+    
+        <!-- Form Name -->
+        <!-- <legend>Contato</legend> -->
+        
+        <!-- Text input-->
+        <div class="form-group">
+          <label class="col-md-4 control-label" for="nome">Nome:</label>  
+          <div class="col-md-4">
+          <input id="nome" name="nome" placeholder="Digite seu nome" class="form-control input-md" required="" type="text">
+            
+          </div>
+        </div>
+        
+        <!-- Text input-->
+        <div class="form-group">
+          <label class="col-md-4 control-label" for="email">E-mail:</label>  
+          <div class="col-md-4">
+          <input id="email" name="email" placeholder="Digite seu e-mail" class="form-control input-md" required="" type="text">
+          <span class="help-block">example@site.com</span>  
+          </div>
+        </div>
+        
+        <!-- Text input-->
+        <div class="form-group">
+          <label class="col-md-4 control-label" for="assunto">Assunto:</label>  
+          <div class="col-md-4">
+          <input id="assunto" name="assunto" placeholder="Assunto da mensagem" class="form-control input-md" required="" type="text">
+            
+          </div>
+        </div>
+        
+        <!-- Textarea -->
+        <div class="form-group">
+          <label class="col-md-4 control-label" for="msg">Digite sua mensagem:</label>
+          <div class="col-md-4">                     
+            <textarea class="form-control" id="msg" name="msg"></textarea>
+          </div>
+        </div>
+        
+        <!-- Button (Double) -->
+        <div class="form-group">
+          <label class="col-md-4 control-label" for="submit"></label>
+          <div class="col-md-8">
+            <button id="submit" name="submit" class="btn btn-primary">Enviar!</button>
+            <button id="limpa" name="limpa" class="btn btn-default">Limpar</button>
+          </div>
+        </div>
+    
+    </fieldset>
+</form>
