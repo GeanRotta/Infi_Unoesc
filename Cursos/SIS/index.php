9ae7ec3997425d94b2496140b1bdac69d377b791
Felipe André Zeiser <felipe_zei@hotmail.com>
2014-09-12 02:18:05 -0300

    V0.0.8
    Trabalhando na Diagramação dos Conteúdos

--- Cursos/SIS/index.php
+++ Cursos/SIS/index.php
@@ -1,2 +1,33 @@
 ﻿<!-- Arquivo index php para paginas inf unoesc -->
-sistemas
+<div align="center">
+             <div class="panel panel-primary">
+                <div class="panel-heading">
+                    <h2 class="panel-title">Sistemas de Informação</h2>
+                </div>
+            <div class="panel-body">
+            	<div align="center">
+            		<a href="#"><img src="Imagens/Sistemas_Informacao.jpg" width="70%" /></a>
+                </div>
+            	<h2 align="center">O Curso</a></h2>
+            	<h4 align="justify">
+                	Este é o curso para quem quer criar soluções com base na tecnologia da informação. Essa escolha leva o aluno ao domínio dos sistemas de hardware, software, dados e procedimentos das redes de computadores de uma organização.  O profissional planeja e organiza os processos que envolvem as informações das empresas. E, além da construção de softwares, poderá planejar, gerenciar e controlar infraestruturas das Tecnologias da Informação e Comunicação (TIC) das organizações.   O curso forma um profissional focado na gestão, no desenvolvimento, na utilização e na avaliação de tecnologias de informação e comunicação aplicadas nas organizações. 
+            	</h4>
+                <h2 align="center">Mercado de Trabalho</a></h2>
+            	<h4 align="justify">
+                	O profissional que domina as tecnologias da informação e comunicação pode optar por diferentes caminhos: desenvolver e analisar sistemas, gerenciar o desenvolvimento, a tecnologia e a infraestrutura da informação, gerenciar sistemas e equipes de trabalho, prestar consultoria em desenvolvimento e análise de sistemas e modelagem de dados de uma empresa, entre outros. Pode atuar em empresas públicas, privadas ou abrir o próprio negócio.
+                </h4>
+                <h2 align="center">Informações</a></h2>
+            	<h4 align="justify">
+                	<h3 align="left">Coordenador</h3>
+                    	Tiago Zonta
+                        <a href="#">tiago.zonta@unoesc.edu.br</a>
+                    <h3 align="left">Duração</h3>
+                    	9 Fases
+                    <h3 align="left">Mensalidade:</h3>
+                    	Valor do Crédito: R$ 31.96
+						*O valor da mensalidade corresponde ao valor unitário do crédito multiplicado pela quantidade de créditos que você cursar.
+                </h4>
+            </div>
+            </div>
+        </div>
+        </div>
--- Home/index.php
+++ Home/index.php
@@ -39,19 +39,19 @@
          <div align="center">
              <div class="panel panel-primary">
                 <div class="panel-heading">
-                    <h2 class="panel-title">Pagina principal</h2>
+                    <h2 class="panel-title">Os Cursos</h2>
                 </div>
             <div class="panel-body">
             	<div align="center">
-            		<a href="#"><img src="Imagens/Angulo01-final--logo-ajustada.jpg" height="50%" width="50%" alt="Novo Bloco para os Cursos de Engenharia da Computação e Sistemas de Informação"/></a>
+            		<a href="#"><img src="Imagens/4_laboratorio_de_informatica.jpg" width="70%" /></a>
                 </div>
-            	<h2 align="center"><a href=""> Muda esse conteudo </a></h2>
+            	<h2 align="center"><a href="http://localhost/docs/Infi_Unoesc/?p=Cursos/ENG"> Engenharia da Computação </a></h2>
             	<h4 align="justify">
-                	bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla  bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla 
+                	Este é o curso para quem tem fascínio por sistemas computacionais, podendo trabalhar com dispositivos eletrônicos (hardware) capazes de processar informações de acordo com um programa (software). A Unoesc forma um profissional com conhecimento em engenharia, especialmente em sistemas computacionais, capaz de planejar, projetar, desenvolver e implementar sistemas eletrônicos, integrando software e hardware para automação de sistemas em geral. 
             	</h4>
-                <h2 align="center"><a href="" > Muda isso também </a></h2>
+                <h2 align="center"><a href="http://localhost/docs/Infi_Unoesc/?p=Cursos/SIS"> Sistemas de Informação </a></h2>
                 <h4 align="justify">
-                	bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla 
+                	Este é o curso para quem quer criar soluções com base na tecnologia da informação. Essa escolha leva o aluno ao domínio dos sistemas de hardware, software, dados e procedimentos das redes de computadores de uma organização.  O profissional planeja e organiza os processos que envolvem as informações das empresas. E, além da construção de softwares, poderá planejar, gerenciar e controlar infraestruturas das Tecnologias da Informação e Comunicação (TIC) das organizações.   O curso forma um profissional focado na gestão, no desenvolvimento, na utilização e na avaliação de tecnologias de informação e comunicação aplicadas nas organizações.
                 </h4>
             </div>
             </div>
