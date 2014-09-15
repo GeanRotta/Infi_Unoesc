099d81a32f9ac1069bdb38a36007b38545f594b0
Felipe André Zeiser <felipe_zei@hotmail.com>
2014-09-15 16:27:28 -0300

    V0.0.9
    Alteração NavBar

--- admin/navbar.php
+++ admin/navbar.php
@@ -15,17 +15,9 @@
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
               <ul class="nav navbar-nav">
-                <li class="dropdown">
-                   <a href="<?php echo $link.'?p=Cursos';?>" class="dropdown-toggle" data-toggle="dropdown">Cursos <span class="caret"></span></a>
-                   <ul class="dropdown-menu" role="menu">
-                     <li <?php if ($pagina=="Cursos/SIS"){echo "class=\"active\" ";}?>><a href="<?php echo $link.'?p=Cursos/SIS'; ?>">Sistemas de Informação</a></li>
-                     <li <?php if ($pagina=="Cursos/ENG"){echo "class=\"active\" ";}?>><a href="<?php echo $link.'?p=Cursos/ENG'; ?>">Engenharia da Computação</a></li>
-                     <li <?php if ($pagina=="Cursos/INF_EST"){echo "class=\"active\" ";}?>><a href="<?php echo $link.'?p=Cursos/INF_EST'; ?>">Infraestrutura dos Cursos</a></li>
-                   </ul>
-                 </li>
+                <li <?php if (strstr($pagina,"Noticias")){echo "class=\"active\" ";}?>><a href="<?php echo $caminho.'?p=Noticias/show_notice'; ?>">Notícias</a></li>
                 <li <?php if (strstr($pagina,"Eventos")){echo "class=\"active\" ";}?>><a href="<?php echo $caminho.'?p=Eventos/show_events';?>">Eventos</a></li>
                 <li <?php if (strstr($pagina,"Empregos")){echo "class=\"active\" ";}?>><a href="<?php echo $caminho.'?p=Empregos/show_vagas';?>">Vagas de Empregos</a></li>
-                <li <?php if (strstr($pagina,"Noticias")){echo "class=\"active\" ";}?>><a href="<?php echo $caminho.'?p=Noticias/show_notice'; ?>">Notícias</a></li>
                 <li <?php if (strstr($pagina,"Contato")){echo "class=\"active\" ";}?>><a href="<?php echo $caminho.'?p=Contato/show_mail'; ?>">E-mails</a></li>
                 <li <?php if (strstr($pagina,"Usuarios")){echo "class=\"active\" ";}?>><a href="<?php echo $caminho.'?p=Usuarios/show_user'; ?>">Usuários</a></li>
                 <li class="navbar-right"><a href="<?php echo $caminho.'?p=logout'; ?>" >Sair</a></li>
